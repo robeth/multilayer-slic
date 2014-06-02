904fcaeb7f006c3a87f4f091d87a021f6373988d
fruity <fruity@fruity-go>
2014-06-02 23:31:20 +0700

    Add femur & humerus parameter extraction modules

--- src/groundtruth/__init__.py
+++ src/groundtruth/__init__.py
@@ -3,17 +3,34 @@ from skimage.io import imsave
 from util.file import get_files, read_image
 from os.path import basename
  
-gt_raw_dir = "data/groundtruth/Ellipse-Raw/"
-gt_ano_dir = "data/groundtruth/Ellipse-Ano/"
-combined_dir = "data/groundtruth/combined/"
+gt_dir = "data/groundtruth/"
+gt_raw_dir = gt_dir + "Ellipse-Raw/"
+gt_ano_dir = gt_dir + "Ellipse-Ano/"
+combined_dir = gt_dir + "combined/"
 comb_head_dir = combined_dir + "comb_head/"
 comb_abdo_dir = combined_dir + "comb_abdo/"
 head_anno_filename = "head_points.txt"
-head_anno_path = combined_dir + head_anno_filename
+head_anno_path = gt_dir + head_anno_filename
+line_mark_filename = "line_mark.txt"
+line_mark_path = gt_dir + line_mark_filename
 
-raw_filenames = get_files(gt_raw_dir)
-ano_filenames = get_files(gt_ano_dir)
-combined_filenames = get_files(combined_dir)
+gt_raw_line_dir = gt_dir + "Line-Raw/"
+gt_ano_line_dir = gt_dir + "Line-Ano/"
+combined_line_dir = gt_dir + "combined-line/"
+comb_femur_dir = combined_line_dir + "comb-femur/"
+comb_humerus_dir = combined_line_dir + "comb-humerus/"
+femur_anno_filename = "femur-result.txt"
+humerus_anno_filename = "humerus-result.txt"
+femur_anno_filepath = gt_dir + femur_anno_filename
+humerus_anno_filepath = gt_dir + humerus_anno_filename
+
+ellipse_raw_filenames = get_files(gt_raw_dir)
+ellipse_ano_filenames = get_files(gt_ano_dir)
+ellipse_combined_filenames = get_files(combined_dir)
+
+line_raw_filenames = get_files(gt_raw_line_dir)
+line_ano_filenames = get_files(gt_ano_line_dir)
+line_combined_filenames = get_files(combined_line_dir)
 
 def get_identifier(filename):
     return filename[0:-5]
@@ -36,12 +53,12 @@ def stack_image(im_path1, im_path2):
     comb_im[im1.shape[0]:, 0:im2.shape[1]] = im2[:]
     return comb_im
 
-def get_pair(raw_filenames, ano_filenames):
+def get_pair(raw_filenames, ano_filenames, raw_parent_dir, ano_parent_dir):
     pair = []
     for raw_filename in raw_filenames:
         similar_filename = find_similar(raw_filename, ano_filenames)
         if similar_filename is not None:
-            new_pair = (gt_raw_dir + raw_filename, gt_ano_dir + similar_filename)
+            new_pair = (raw_parent_dir + raw_filename, ano_parent_dir + similar_filename)
             pair.append(new_pair)
     return pair
 
@@ -51,29 +68,35 @@ def find_in_pair(query, pairs, index):
             return pair
     return None
 
-def ROUTINE_create_stack_images():
-    pairs = get_pair(raw_filenames, ano_filenames)
+def ROUTINE_create_head_stack_images():
+    pairs = get_pair(ellipse_raw_filenames, ellipse_ano_filenames, gt_raw_dir, gt_ano_dir)
     for pair in pairs:
         combined_image = stack_image(pair[0], pair[1])
         imsave(combined_dir + basename(pair[0]), combined_image)
+        
+def ROUTINE_create_line_stack_images():
+    pairs = get_pair(line_raw_filenames, line_ano_filenames, gt_raw_line_dir, gt_ano_line_dir)
+    for pair in pairs:
+        combined_image = stack_image(pair[0], pair[1])
+        imsave(combined_line_dir + basename(pair[0]), combined_image)
     
-def ROUTINE_delnonpair(delete=False):
-    pairs = get_pair(raw_filenames, ano_filenames)
+def ROUTINE_head_delnonpair(delete=False):
+    pairs = get_pair(ellipse_raw_filenames, ellipse_ano_filenames, gt_raw_dir, gt_ano_dir)
     # del_raw
-    for raw_filename in raw_filenames:
+    for raw_filename in ellipse_raw_filenames:
         raw_filename = gt_raw_dir + raw_filename
         pair_result = find_in_pair(raw_filename, pairs, 0)
         if (pair_result is None) or (pair_result[1] is None):
             print "Delete Raw:" + raw_filename
     print "----"
-    for ano_filename in ano_filenames:
+    for ano_filename in ellipse_ano_filenames:
         ano_filename = gt_ano_dir + ano_filename
         pair_result = find_in_pair(ano_filename, pairs, 1)
         if pair_result is None:
             print "Delete Ano:" + ano_filename
             
 def ROUTINE_split_head_abdo():
-    for combined_filename in combined_filenames:
+    for combined_filename in ellipse_combined_filenames:
         combined_image = read_image(combined_dir + combined_filename, dir="")
         image_shape = combined_image.shape
         head_image = combined_image[:, 0:int(image_shape[1] / 2)]
@@ -81,7 +104,9 @@ def ROUTINE_split_head_abdo():
         imsave(comb_head_dir + combined_filename, head_image)
         imsave(comb_abdo_dir + combined_filename, abdo_image)
 
-def annotate_ellipse(input_image, filename, output_filename, tag, params=None, save=True, draw_ellipse=False, mode=1):
+def annotate_ellipse(input_image, filename, output_filename, tag, params=None, save=True):
+    from skimage.draw import ellipse_perimeter
+    import matplotlib.pyplot as plt
     global in_image
     global in_filename
     
@@ -89,14 +114,15 @@ def annotate_ellipse(input_image, filename, output_filename, tag, params=None, s
     in_filename = filename
     display_image = np.copy(input_image)
     
-    if draw_ellipse:
-        from skimage.draw import ellipse_perimeter
+    if params:
         r_index, c_index = ellipse_perimeter(params[0], params[1],
-                                             params[2], params[3],
-                                             params[4])
+                                                 params[2], params[3],
+                                                 float(params[4]))
         display_image[r_index, c_index] = np.array([0, 0, 200])
     
-    import matplotlib.pyplot as plt
+    
+    
+    
     global fig
     global plt
     
@@ -117,7 +143,7 @@ def annotate_ellipse(input_image, filename, output_filename, tag, params=None, s
     points = [[0, 0], [0, 0]]
     is_select_axis = True
     semi_minor = 5
-    ellipse_params = [0,0,0,0,0]
+    ellipse_params = [0, 0, 0, 0, 0]
     
         
     def redraw():
@@ -148,7 +174,6 @@ def annotate_ellipse(input_image, filename, output_filename, tag, params=None, s
         global is_select_axis
         global semi_minor
         global ellipse_params
-        print "you pressed button: %s" % (str(event.button))
         
         def find_length(y1, y2, x1, x2):
             from math import sqrt, pow
@@ -157,7 +182,7 @@ def annotate_ellipse(input_image, filename, output_filename, tag, params=None, s
             return [(y1 + y2) / 2, (x1 + x2) / 2]
         def find_rad(y1, y2, x1, x2):
             from math import radians
-            if x1 == x2: print "EQUALS"; return radians(90)
+            if x1 == x2: return radians(90)
             else:
                 return float(y1 - y2) / float(x1 - x2)
                 
@@ -178,7 +203,7 @@ def annotate_ellipse(input_image, filename, output_filename, tag, params=None, s
             center_y, center_x = find_center(points[0][0], points[1][0], points[0][1], points[1][1])
             if not is_select_axis:
                 semi_minor = find_length(center_y, y, center_x, x)
-            semi_major = find_length(points[0][0], points[1][0], points[0][1], points[1][1])/2
+            semi_major = find_length(points[0][0], points[1][0], points[0][1], points[1][1]) / 2
             from math import atan
             radi = atan(find_rad(points[0][0], points[1][0], points[0][1], points[1][1]))
             ellipse_params = [int(center_y), int(center_x), int(semi_minor), int(semi_major), radi]
@@ -188,8 +213,7 @@ def annotate_ellipse(input_image, filename, output_filename, tag, params=None, s
         global points
         global ellipse_params
         global in_filename
-        if event.key == "enter":
-            print points
+        if event.key == "enter" or event.key == "shift":
             if save:
                 f = open(combined_dir + output_filename , 'a')
                 f.write(in_filename + ",")
@@ -197,8 +221,96 @@ def annotate_ellipse(input_image, filename, output_filename, tag, params=None, s
                     f.write(str(e_param) + ",")
                 f.write(tag + "\n")
                 f.close()
+                print "%s:%s" % (in_filename, str(ellipse_params))
             plt.close('all')
+    
+    if params:
+        cid = fig.canvas.mpl_connect('button_press_event', click)
+        cid = fig.canvas.mpl_connect('key_press_event', on_key)
+
+    mng = plt.get_current_fig_manager()
+    mng.resize(*mng.window.maxsize())
+    plt.show()
+
+def annotate_line(input_image, filename, output_filename, tag, params=None, save=True):
+    from skimage.draw import line
+    import matplotlib.pyplot as plt
+    global in_image
+    global in_filename
+    global fig
+    global plt
+    
+    in_image = input_image
+    in_filename = filename
+    display_image = np.copy(input_image)
+    
+    if params:
+        r_index, c_index = line(params[0], params[1],
+                                                 params[2], params[3])
+        display_image[r_index, c_index] = np.array([0, 0, 200])
+    
+    fig, ax = plt.subplots(1, 1)
+    fig.set_size_inches(8, 4, forward=True)
+    plt.subplots_adjust(0.05, 0.05, 0.95, 0.95, 0.05, 0.05)
+    pltcop = ax.imshow(display_image)
+    ax.set_xticks(())
+    ax.set_yticks(())
+    
+    global click_counter
+    global is_select_axis
+    global points
+    global semi_minor
+    global ellipse_params
+    
+    click_counter = 0
+    points = [[0, 0], [0, 0]]
+        
+    def redraw():
+        global points
+        global in_image
+        global plt
+        global fig
+        
+        display_image = np.copy(in_image)
+        
+        r_index, c_index = line(points[0][0], points[0][1], points[1][0], points[1][1])
+        display_image[r_index, c_index] = np.array([200, 0, 0])
+        
+        pltcop.set_data(display_image)
+        fig.canvas.draw()
+    
+    def click(event):
+        global click_counter
+        global points
+        
+        def find_length(y1, y2, x1, x2):
+            from math import sqrt, pow
+            return sqrt(pow(y1 - y2, 2) + pow(x1 - x2, 2))
                 
+        if event.button == 1:
+            if(event.xdata is None): return
+            
+            x = int(event.xdata)
+            y = int(event.ydata)
+            points[click_counter] = [y, x]
+            click_counter += 1
+            if click_counter >= len(points):
+                click_counter = 0  
+            redraw()
+    
+    def on_key(event):
+        global points
+        global ellipse_params
+        global in_filename
+        if event.key == "enter" or event.key == "shift":
+            if save:
+                f = open(gt_dir + output_filename , 'a')
+                entry = ("%s,%s,%s,%s,%s,%s\n")%(in_filename, points[0][0], points[0][1], points[1][0], points[1][1], tag)
+                f.write(entry)
+                f.close()
+                print entry
+            plt.close('all')
+    
     cid = fig.canvas.mpl_connect('button_press_event', click)
     cid = fig.canvas.mpl_connect('key_press_event', on_key)
 
@@ -238,7 +350,7 @@ def read_anno_result(anno_path):
     return anno_dictionary
 
 def validate_ellipse_anno(image, filename, params):
-    annotate_ellipse(image, filename, "head_points.txt", "head", params, True, True)
+    annotate_ellipse(image, None, None, None, params, False)
 
 def ROUTINE_check_head_anno():
     params_dictionary = read_anno_result(head_anno_path)
@@ -246,40 +358,116 @@ def ROUTINE_check_head_anno():
     for head_filename in head_filenames:
         head_image = read_image(head_filename, dir=comb_head_dir)
         head_image = get_anno_image(head_image)
-        validate_ellipse_anno(head_image, head_filename, params_helper(params_dictionary[head_filename]))
+        validate_ellipse_anno(head_image, head_filename, params_dictionary[head_filename])
 
-def params_helper(params):
-    return get_ellipse_params(params[0], params[1], params[2], params[3], params[4], params[5], params[6], params[7])
-        
-def get_ellipse_params(y1, x1, y2, x2, y3, x3, y4, x4):
-    from shapely.geometry import LineString
-    from math import radians, degrees, atan
-    line1 = LineString([(x1, y1), (x2, y2)])
-    line2 = LineString([(x3, y3), (x4, y4)])
+def mark_line(input_image, filename):
+    global in_image
+    global in_filename
+    global fig
+    global pltcop
+    global display_image
     
-    minor = major = None
-    rad = degrees(90)
+    in_image = input_image
+    in_filename = filename
     
-    if line1.length > line2.length:
-        major = line1
-        minor = line2
-    else:
-        major = line2
-        minor = line1
-        
-    intersection_point = line1.intersection(line2)
-    major_point1_x, major_point1_y = major.coords[0]
-    major_point2_x, major_point2_y = major.coords[1]
+    import matplotlib.pyplot as plt
     
-    if major_point1_x != major_point2_y:
-        tan_value = (major_point1_y - major_point2_y) / (major_point1_x - major_point2_x)
-        rad = atan(tan_value)
+    fig, ax = plt.subplots(1, 1)
+    fig.set_size_inches(8, 4, forward=True)
+    plt.subplots_adjust(0.05, 0.05, 0.95, 0.95, 0.05, 0.05)
+    pltcop = ax.imshow(input_image)
+    ax.set_xticks(())
+    ax.set_yticks(())
+    
+    global is_hc
+    is_hc = False
+    
+    def redraw():
+        global in_image
+        global display_image
+        global pltcop
+        global fig
+        global is_hc
         
-    ellipse_params = [int(intersection_point.y), int(intersection_point.x), int(minor.length / 2), int(major.length / 2), rad]
-    print ellipse_params
-    return ellipse_params
+        display_image = np.copy(in_image)
+        
+        if is_hc:
+            from skimage.draw import circle
+            r_index, c_index = circle(100, 100, 25)
+            display_image[r_index, c_index] = np.array([200, 0, 0])
+        pltcop.set_data(display_image)
+        fig.canvas.draw()
+        
+    def click(event):
+        global is_hc
+                
+        if event.button == 3 or event.button == 1:
+            is_hc = not is_hc
+        redraw()
+    
+    def on_key(event):
+        global in_filename
+        global is_hc
+        if event.key == "enter" or event.key == "shift":
+            f = open(line_mark_path , 'a')
+            f.write(in_filename + "," + str(is_hc) + "\n")
+            f.close()
+            plt.close('all')
+    
+    cid = fig.canvas.mpl_connect('button_press_event', click)
+    cid = fig.canvas.mpl_connect('key_press_event', on_key)
 
-# ROUTINE_create_stack_images()
-# ROUTINE_split_head_abdo()
-# ROUTINE_annotate_head()
-ROUTINE_check_head_anno()
+    mng = plt.get_current_fig_manager()
+    mng.resize(*mng.window.maxsize())
+    plt.show()
+
+def ROUTINE_mark_line():
+    line_pairs = get_pair(line_raw_filenames, line_ano_filenames , gt_raw_line_dir, gt_ano_line_dir)
+    for line_pair in line_pairs:
+        full_line_image = stack_image(line_pair[0], line_pair[1])
+        from os.path import basename
+        print line_pair
+        mark_line(full_line_image, basename(line_pair[0]))
+        
+def read_line_config():
+    config_file = open(line_mark_path, "r")
+    lines = config_file.readlines()
+    config_dictionary = {}
+    for line in lines:
+        parts = line.split(",")
+        parts[1] = parts[1].replace("\n", "")
+        config_dictionary[parts[0]] = parts[1] == "True"
+    return config_dictionary
+
+def ROUTINE_split_femur_humerus():
+    config_dictionary = read_line_config()
+    for combined_filename in line_combined_filenames:
+        combined_image = read_image(combined_line_dir + combined_filename, dir="")
+        image_shape = combined_image.shape
+        image1 = combined_image[:, 0:int(image_shape[1] / 2)]
+        image2 = combined_image[:, int(image_shape[1] / 2):]
+        if config_dictionary[combined_filename]:
+            imsave(comb_femur_dir + combined_filename, image2)
+            imsave(comb_humerus_dir + combined_filename, image1)
+        else:
+            imsave(comb_femur_dir + combined_filename, image1)
+            imsave(comb_humerus_dir + combined_filename, image2)
+            
+def SUBROUTINE_annotate_line(directory, output_filename, tag):
+    filenames = get_files(directory)
+    for filename in filenames:
+        line_image = read_image(filename, dir=directory)
+        line_image = get_anno_image(line_image)
+        annotate_line(line_image, filename, output_filename, tag)
+
+def ROUTINE_annotate_femur():
+    SUBROUTINE_annotate_line(comb_femur_dir, femur_anno_filename, "femur")
+    
+def ROUTINE_annotate_humerus():
+    SUBROUTINE_annotate_line(comb_humerus_dir, humerus_anno_filename, "humerus")
+        
+# ROUTINE_mark_line()
+# ROUTINE_create_line_stack_images()
+# read_line_config()
+# ROUTINE_split_femur_humerus()
+ROUTINE_annotate_humerus()
